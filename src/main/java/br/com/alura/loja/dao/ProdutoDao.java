a3e38b9be37ec5d184dfea72bd390a2c6e937584
Anderson <andso.alves@gmail.com>
2022-08-21 15:12:43 -0300

    Finalizando JPA

--- src/main/java/br/com/alura/loja/dao/ProdutoDao.java
+++ src/main/java/br/com/alura/loja/dao/ProdutoDao.java
@@ -4,16 +4,57 @@ import javax.persistence.EntityManager;
 
 import br.com.alura.loja.modelo.Produto;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public class ProdutoDao {
-	
+
 	private EntityManager em;
 
 	public ProdutoDao(EntityManager em) {
 		this.em = em;
 	}
-	
+
 	public void cadastrar(Produto produto) {
 		this.em.persist(produto);
 	}
 
+	public void atualizar(Produto produto) {
+		this.em.merge(produto);
+	}
+
+	public void remover(Produto produto) {
+		produto = em.merge(produto);
+		this.em.remove(produto);
+	}
+
+	public Produto buscarPorId(Long id) {
+		return em.find(Produto.class, 1L);
+	}
+
+	public List<Produto> buscarTodos() {
+		String jpql = "SELECT p FROM Produto AS p";
+		return em.createQuery(jpql, Produto.class).getResultList();
+	}
+
+	public List<Produto> buscarPorNome(String nome) {
+		String jpql = "SELECT p FROM Produto AS p WHERE p.nome = ?1";
+		return em.createQuery(jpql, Produto.class)
+				.setParameter(1, nome)
+				.getResultList();
+	}
+
+	public List<Produto> buscarPorNomeDaCategoria(String nome) {
+		String jpql = "SELECT p FROM Produto AS p WHERE p.categoria.nome = ?1";
+		return em.createQuery(jpql, Produto.class)
+				.setParameter(1, nome)
+				.getResultList();
+	}
+
+	public BigDecimal buscarPrecoPorNome(String nome) {
+		String jpql = "SELECT p.preco FROM Produto p WHERE p.nome = :nome";
+		return em.createQuery(jpql, BigDecimal.class)
+				.setParameter("nome", nome)
+				.getSingleResult();
+	}
 }
--- src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -1,6 +1,7 @@
 package br.com.alura.loja.testes;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 
@@ -13,18 +14,37 @@ import br.com.alura.loja.util.JPAUtil;
 public class CadastroDeProduto {
 	
 	public static void main(String[] args) {
+		cadastrarProduto();
+		EntityManager em = JPAUtil.getEntityManager();
+		ProdutoDao produtoDao = new ProdutoDao(em);
+
+		Produto p = produtoDao.buscarPorId(1l);
+		System.out.println(p.getPreco());
+
+		List<Produto> todos = produtoDao.buscarPorNome("Xiaomi Redmi");
+		todos.forEach(p2 -> System.out.println(p.getNome()));
+
+		List<Produto> todos3 = produtoDao.buscarPorNomeDaCategoria( "CELULARES");
+		todos3.forEach(p3 -> System.out.println(p.getNome()));
+
+		BigDecimal varPrecoProduto = produtoDao.buscarPrecoPorNome("Xiaomi Redmi");
+		System.out.println("Por Preco " + varPrecoProduto);
+
+	}
+
+	private static void cadastrarProduto() {
 		Categoria celulares = new Categoria("CELULARES");
 		Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), celulares );
-		
+
 		EntityManager em = JPAUtil.getEntityManager();
 		ProdutoDao produtoDao = new ProdutoDao(em);
 		CategoriaDao categoriaDao = new CategoriaDao(em);
-		
+
 		em.getTransaction().begin();
-		
+
 		categoriaDao.cadastrar(celulares);
 		produtoDao.cadastrar(celular);
-		
+
 		em.getTransaction().commit();
 		em.close();
 	}
